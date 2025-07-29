32d283a40ef6877d638dabeb5e1881e3271fe9eb
wonswe <64626054+wonswe@users.noreply.github.com>
2025-07-28 21:14:20 -0400

    cont. mouseover

--- find_coordinates.js
+++ find_coordinates.js
@@ -2,3 +2,16 @@ const html = document.querySelector('html');
 html.addEventListener('click', (event) => {
   console.log(`client: ${event.clientX}, ${event.clientY}`);
 });
+
+let intervalId;
+
+html.addEventListener('mouseover', function (event) {
+  intervalId = setInterval(() => {
+    console.log(`client: ${event.clientX}, ${event.clientY}`);
+  }, 100);
+});
+
+html.addEventListener('mouseout', function () {
+  clearInterval(intervalId);
+  console.log('Mouse left the window.');
+});
