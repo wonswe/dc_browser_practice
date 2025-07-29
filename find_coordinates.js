de79686acd85ea6a4633562f741bed59878312d5
wonswe <64626054+wonswe@users.noreply.github.com>
2025-07-28 21:48:26 -0400

    complete basic functions

--- find_coordinates.js
+++ find_coordinates.js
@@ -1,19 +1,31 @@
 const html = document.querySelector('html');
 let latestX = 0;
 let latestY = 0;
-let intervalId;
+// let intervalId;
+
+const vertical = document.getElementById('vertical');
+const horizontal = document.getElementById('horizontal');
+const coordinates = document.getElementById('coordinates');
 
 html.addEventListener('mousemove', function (event) {
   latestX = event.clientX;
   latestY = event.clientY;
 });
 
-html.addEventListener('mouseover', function () {
-  intervalId = setInterval(() => {
-    console.log(`client: ${latestX}, ${latestY}`);
-  }, 100);
+document.addEventListener('mousemove', (event) => {
+  vertical.style.left = `${event.clientX}px`;
+  horizontal.style.top = `${event.clientY}px`;
+  coordinates.style.left = `${event.clientX}px`;
+  //   coordinates.style.top = `${event.clientY + 20}px`;
+  coordinates.textContent = `${event.clientX}px, ${event.clientY}px`;
 });
 
-html.addEventListener('mouseout', function () {
-  clearInterval(intervalId);
-});
+// html.addEventListener('mouseover', function () {
+//   intervalId = setInterval(() => {
+//     console.log(`client: ${latestX}, ${latestY}`);
+//   }, 10);
+// });
+
+// html.addEventListener('mouseout', function () {
+//   clearInterval(intervalId);
+// });
