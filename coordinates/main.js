386e642795549401fb56b0288faa0008c6f76024
wonswe <64626054+wonswe@users.noreply.github.com>
2025-07-30 22:45:21 -0400

    cont. dc_solution

---
+++ coordinates/main.js
@@ -0,0 +1,4 @@
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
