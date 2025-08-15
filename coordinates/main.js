8437e57e5c1484ff2477880fa768bc581b428070
wonswe <64626054+wonswe@users.noreply.github.com>
2025-08-14 23:59:42 -0700

    cont. 4.8

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,6 +2,9 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 document.addEventListener('mousemove', (event) => {
   const x = event.clientX;
@@ -9,10 +12,9 @@ document.addEventListener('mousemove', (event) => {
 
   vertical.style.transform = `translateX(${x}px)`;
   horizontal.style.transform = `translateY(${y}px)`;
-
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${
+    y - targetHalfHeight
+  }px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
   tag.innerHTML = `${x}px, ${y}px`;
 });
